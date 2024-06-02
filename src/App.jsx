b9aa9f4b077c5f9a228090c2fe32efe68d7e7e83
rivershertz <river16@shertz.com>
2024-06-03 00:05:30 +0300

    remove key from public

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import {initMap} from './utils/initMap';
 
 export default function App() {
-  const apiKey = 'AIzaSyAfjgo88kqQ2H-82d4NophQP9iKbaIGQH8';
-
   initMap();
   return (
     <div className="App">
